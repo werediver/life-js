d92d0a59835e6e591fa0f55efd850b7f111d8cf3
werediver <werediver@gmail.com>
2013-01-15 15:30:40 +0300

    Cell age is shown through change of color.

--- js/life-graphics.js
+++ js/life-graphics.js
@@ -1,3 +1,51 @@
+function componentToHex(x) {
+    var hex = x.toString(16);
+    return hex.length == 2 ? hex : '0' + hex;
+}
+
+function hexColorFromComponents(components) {
+	var r = components[0];
+	var g = components[1];
+	var b = components[2];
+    return '#' + componentToHex(r) + componentToHex(g) + componentToHex(b);
+}
+
+function hexColorToComponents(hexColor) {
+	var re = /#([0-9A-F]{1,2})([0-9A-F]{2})([0-9A-F]{2})/i;
+	var tokens = re.exec(hexColor);
+
+	var r = parseInt(tokens[1], 16);
+	var g = parseInt(tokens[2], 16);
+	var b = parseInt(tokens[3], 16);
+
+	return [r, g, b];
+}
+
+function hexColorAdd(hexColor, extra, bounds) {
+	var components = hexColorToComponents(hexColor);
+	for (var i = 0; i < 3; ++i) {
+		var x = components[i] + extra[i];
+		var xMin = bounds[i][0];
+		var xMax = bounds[i][1];
+
+		if (x > xMax)
+			x = xMax;
+		else if (x < xMin)
+			x = xMin;
+
+		components[i] = x;
+	}
+	return hexColorFromComponents(components);
+}
+
+function colorBounds(dark, light) {
+	var components1 = hexColorToComponents(dark);
+	var components2 = hexColorToComponents(light);
+	return [[components1[0], components2[0]],
+			[components1[1], components2[1]],
+			[components1[2], components2[2]]];
+}
+
 var LifeGraphics = (function () {
 	function LifeGraphics(canvas, cellSize) {
 		this.canvas  = canvas;
@@ -39,11 +87,16 @@ var LifeGraphics = (function () {
 	};
 
 	LifeGraphics.prototype.drawCells = function (core) {
-		this.context.fillStyle = "#80F080";
+		// TODO: _Optionally_ show cell age through change of the color.
+		//this.context.fillStyle = "#80F080";
+		var fillColor = "#80F080";
+		var bounds = colorBounds("#60A060", "#FFFFFF");
 		var dgrid = core.dgrid;
 		for (var col = 0; col < dgrid.width; ++col) {
 			for (var row = 0; row < dgrid.height; ++row) {
-				if (dgrid.get(col, row) > 0) {
+				var cell = dgrid.get(col, row);
+				if (cell > 0) {
+					this.context.fillStyle = hexColorAdd(fillColor, [-cell * 10, -cell * 10, -cell * 10], bounds);
 					// Here is hard-coded 1 px border
 					this.context.fillRect(
 							this.xoffset + this.cellSize * col + 1,
